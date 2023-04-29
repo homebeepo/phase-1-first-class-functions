21008fa7e831e58c1b6f0109caf7342bef060449
caseyclinch <clinchcasey@gmail.com>
2023-04-28 18:20:25 -0700

    Completed Project

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function receivesAFunction(satanworship) {
+    satanworship()
+}
+
+function returnsANamedFunction() {
+    return function saySomething() {
+        console.log('Something');
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+        console.log('nothing')
+    }
+}
